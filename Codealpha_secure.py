05e780b2374fb940bd54446ac715c0d87a2cf9ce
COSMAS-ND <osmas.nnadube@gmail.com>
2026-08-27 00:56:43 +0100

    Delete Codealpha_secure.py

--- Codealpha_secure.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect(":memory:")
-cursor = conn.cursor()
-
-username = input("Enter username: ")
-cursor.execute("SELECT * FROM users WHERE username = ?" , (username, ))
-print("Query executed safely")
